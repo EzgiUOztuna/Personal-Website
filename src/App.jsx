7e8103e839a23c35e4f366fdf52d3c6e6c7d6e43
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2024-12-27 20:28:37 +0300

    İlk commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Introduction from "./components/Introduction";
+import Profile from "./components/Profile";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+function App() {
+
+  return (
+    <>
+      <Introduction />
+      <Skills />
+      <Profile />
+      <Projects />
+    </>
+  );
+}
+
+export default App;
