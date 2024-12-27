6c5b0c2416383dea52f9fbc2422d3b32d2919bf2
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2024-12-27 21:10:10 +0300

    Create DummyData.js

---
+++ src/assets/DummyData.js
@@ -0,0 +1,21 @@
+/* ORNEK
+
+const menuData = {
+    "menuTitle": [
+        {
+            "id": 1,
+            "name": "Position Absolute Acı Pizza",
+            "price": 85.50
+        },
+        {
+            "id": 2,
+            "name": "Hackathlon Burger Menü",
+            "price": 75.00
+        }
+    ],
+
+    "ekMalzemelerListesi": ["Pepperoni", "Tavuk Izgara", "Mısır", "Sarımsak", "Ananas", "Sosis", "Soğan", "Sucuk", "Biber", "Kabak", "Kanada Jambonu", "Domates", "Jalepeno"],
+};
+
+export default menuData;
+*/
