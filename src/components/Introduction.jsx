b87718dcea92ab4555c66fcbbc8639246c071331
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2024-12-28 16:41:49 +0300

    Dark mode

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,6 +1,18 @@
-export default function Introduction() {
-    return (
-        <div>Introduction</div>
-    )
-}
+import "./Introduction.css";
+import { useState } from "react";
 
+export default function Introduction() {
+    const [darkMode, setDarkMode] = useState(false);
+    const handleDarkModeChange = () => setDarkMode(!darkMode);
+
+    return (
+        <div className={`background ${darkMode ? "dark" : ""}`}>
+            <input
+                type="checkbox"
+                id="darkmode-toggle"
+                onChange={handleDarkModeChange}
+            />
+            <label htmlFor="darkmode-toggle">Dark Mode</label>
+        </div>
+    );
+}
