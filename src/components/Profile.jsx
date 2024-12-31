7568dfc22722b41860a7e43bf0d308e502591a3a
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2024-12-31 10:56:45 +0300

    Profile_Tailwindcss

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,21 +9,21 @@ export default function Profile() {
 
     return (
         <>
-            <div className={`profile-container ${darkMode ? "dark" : ""}`}>
-                <img className="ellipse-12" src="Ellipse 12.svg" />
-                <h3 className="header-3">{words.profile}</h3>
-                <div className="info-cards">
-                    <div className="basic-info-card">
-                        <h4>{words.basicInformation}</h4>
-                        <p><span className="label">{words.birthDate.label}</span>{words.birthDate.value}</p>
-                        <p><span className="label">{words.city.label}</span>{words.city.value}</p>
-                        <p><span className="label">{words.education.label}</span>{words.education.value}</p>
-                        <p><span className="label">{words.role.label}</span>{words.role.value}</p>
+            <div className={`w-screen h-auto bg-gray-100 relative transition-all block ${darkMode ? "dark:bg-slate-800 text-white" : ""}`}>
+                <img className="absolute right-0 top-[-2rem]" src="Ellipse 12.svg" />
+                <h3 className="text-center font-inter text-[2.25rem] font-medium py-[3rem]">{words.profile}</h3>
+                <div className="flex justify-center items-center gap-16 pb-20">
+                    <div className="border border-white rounded-lg bg-white shadow-[rgba(82,82,82,0.5)] w-[30rem] p-8 ">
+                        <h4 className="font-playfair text-[#ea2678] font-normal text-[1.5rem] pb-[1.5rem] ml-[1rem]">{words.basicInformation}</h4>
+                        <p className="flex justify-start items-baseline my-2 font-inter text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.birthDate.label}</span>{words.birthDate.value}</p>
+                        <p className="flex justify-start items-baseline my-2 font-inter text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.city.label}</span>{words.city.value}</p>
+                        <p className="flex justify-start items-baseline my-2 font-inter text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4 ">{words.education.label}</span>{words.education.value}</p>
+                        <p className="flex justify-start items-baseline my-2 font-inter text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.role.label}</span>{words.role.value}</p>
                     </div>
 
-                    <div className="about-me">
-                        <h4>{words.aboutMe} <img className="rectangle-39" src="Rectangle 39.svg" /></h4>
-                        <p>{words.aboutMeParagraph}</p>
+                    <div className="w-[27.5rem] leading-8 relative">
+                        <h4 className="font-playfair text-2xl font-normal leading-8 relative z-[10] ">{words.aboutMe} <img className="absolute top-[1.1rem] left-[-0.9rem] w-[5.62rem] -z-10" src="Rectangle 39.svg" /></h4>
+                        <p className="relative font-inter text-base font-normal my-6 w-[30rem]">{words.aboutMeParagraph}</p>
                     </div>
                 </div>
             </div>
