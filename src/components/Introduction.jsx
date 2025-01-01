738643f5a8ba01f2eac15e2d6a759d7f083d43aa
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-01-01 18:21:30 +0300

    Introduction Responsive done

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -12,12 +12,18 @@ export default function Introduction() {
 
     return (
         <>
-            <div className={`w-screen bg-gray-100 transition-all from-neutral-300 relative  ${darkMode ? "dark:bg-slate-800 text-white" : ""}`}>
-                <img className="absolute left-[29.23%]" src="Ellipse 10.svg" />
-                <img className="absolute top-[80%] right-0" src="Rectangle 18.svg" />
+            <div className={`w-screen bg-gray-100 transition-all from-neutral-300 relative ${darkMode ? "dark:bg-slate-800 text-white" : ""}`}>
+                <img className="absolute left-[4%] w-20
+                                lg:left-[29.23%] lg:w-[6.93rem]
+                                xl:left-[29.23%] xl:w-[6.93rem]" src="Ellipse 10.svg" />
+                <img className="absolute top-[70%] right-0 w-[62px]
+                                lg:top-[80%] lg:w-[124px]
+                                xl:top-[80%] xl:w-[124px]" src="Rectangle 18.svg" />
                 <img className="absolute left-[68%] top-[37.5rem] z-[1]" src="Ellipse 11.svg" />
-                <div className="flex justify-end h-[1.12rem] gap-8 pt-8 pr-36">
-                    <div className="h-[1.63rem] flex gap-2">
+                <div className="relative flex justify-end h-[1.12rem] gap-2 pt-8 pr-4 items-center 
+                                lg:gap-8 lg:pr-36
+                                xl:gap-8 xl:pr-36">
+                    <div className="h-[1.63rem] flex items-center ">
                         <input
                             type="checkbox"
                             id="darkmode-toggle"
@@ -25,11 +31,19 @@ export default function Introduction() {
                             onChange={handleDarkModeChange}
                             className="peer hidden"
                         />
-                        <label htmlFor="darkmode-toggle" className="w-[3.44rem] h-6 relative flex bg-[#e92577] opacity-75 rounded-[12.5rem] cursor-pointer transition-duration: 300ms; after:content-[''] after:w-4 after:h-4 after:absolute after:top-1 after:left-1 after:bg-[#ffe86e] after:rounded-[11.25rem] after:transition-duration: 300ms; peer-checked:after:left-[3.18rem] peer-checked:after:-translate-x-full peer-checked:after:bg-[#3a3a3a] active:after:w-[1.62rem]"></label>
-                        <p className="h-6 font-inter text-base font-bold text-[#777777]">{language === "en" ? englishWords.mode : turkishWords.mode}</p>
+                        <label htmlFor="darkmode-toggle" className="w-[2rem] h-5 relative flex bg-[#e92577] opacity-75 rounded-[12.5rem] cursor-pointer transition-duration: 300ms;  after:content-[''] after:w-[0.8rem] after:h-[0.8rem] after:absolute after:top-[0.24rem] after:left-1 after:bg-[#ffe86e] after:rounded-[11.25rem] after:transition-duration: 300ms; peer-checked:after:left-[1.8rem] peer-checked:after:-translate-x-full peer-checked:after:bg-[#3a3a3a] active:after:w-[2rem]
+                        lg:w-[3.44rem] lg:h-6 lg:after:w-4 lg:after:h-4 lg:after:top-1 lg:peer-checked:after:left-[3.18rem] lg:active:after:w-[1.62rem] 
+                        xl:w-[3.44rem] xl:h-6 xl:after:w-4 xl:after:h-4 xl:after:top-1 xl:peer-checked:after:left-[3.18rem] xl:active:after:w-[1.62rem]"></label>
+                        <p className="h-6 font-inter text-[0.9rem] font-bold text-[#777777] pl-2
+                                      lg:text-base
+                                      xl:text-base">{language === "en" ? englishWords.mode : turkishWords.mode}</p>
                     </div>
-                    <div className="h-6 font-inter text-base font-bold text-[#777777]">|</div>
-                    <div className="flex items-end h-[1.5rem] font-inter text-[1rem] font-bold text-[#777777]">
+                    <div className="h-6 font-inter text-[0.9rem] font-bold text-[#777777]
+                                    lg:text-base
+                                    xl:text-base">|</div>
+                    <div className="flex items-end  font-inter text-[0.9rem] font-bold text-[#777777]
+                                    lg:h-[1.5rem] lg:text-[1rem]
+                                    xl:h-[1.5rem] xl:text-[1rem]">
                         <button onClick={toggleLanguage} dangerouslySetInnerHTML={{ __html: word.translate }}>
                         </button>
                     </div>
--- src/components/IntroductionDefinition.jsx
+++ src/components/IntroductionDefinition.jsx
@@ -7,23 +7,31 @@ function IntroductionDefinition() {
     const { words } = useContext(LanguageContext);
 
     return (
-        <div className={`flex justify-center items-start pt-20 gap-12 ${darkMode ? "dark:bg-slate-800 text-white" : ""}`}>
-            <div className="relative w-[40rem] font-inter text-[2.62rem] font-medium z-10">
-                <img className="absolute left-[-1.5rem] top-[6.2rem] z-[-1]" src="Rectangle 2.svg" />
-                <p>{words.title}</p>
-                <p>{words.definition}</p>
+        <div className={`flex justify-center items-start pt-20 gap-0 ${darkMode ? "dark:bg-slate-800 text-white" : ""}`}>
+            <div className="relative w-[20rem] font-inter text-[1rem] font-medium z-10
+                            lg:w-[40rem] lg:text-[2.62rem]
+                            xl:w-[40rem] xl:text-[2.62rem]">
+                <img className="absolute left-[4.2rem] top-[2.1rem] z-[-1] h-5 w-[3.75rem]
+                                lg:top-[6.2rem] lg:h-[1.94rem] lg:left-[-1.5rem] lg:w-[9.25rem]
+                                xl:top-[6.2rem] xl:h-[1.94rem] xl:left-[-1.5rem] xl:w-[9.25rem]" src="Rectangle 2.svg" />
+                <p className="w-[12.5rem] ml-20 lg:w-[38.25rem] lg:ml-0 xl:w-[38.25rem] xl:ml-0">{words.title}</p>
+                <p className="w-[12.5rem] ml-20 lg:w-[38.25rem] lg:ml-0 xl:w-[38.25rem] xl:ml-0">{words.definition}</p>
                 <div>
-                    <div className="flex gap-4 h-[35px] items-center pt-20 pb-8">
+                    <div className="flex gap-2.5 h-[2.18rem] items-center pt-9 pb-4 ml-20
+                                    lg:gap-4 lg:h-[2.18rem] lg:pt-20 lg:pb-8 lg:ml-0
+                                    xl:gap-4 xl:h-[2.18rem] xl:pt-20 xl:pb-8 xl:ml-0">
                         {words.socialMedia.map((item, index) => (
                             <a key={index} href={item.url} target="_blank">
                                 <i className={item.iconClass}></i>
                             </a>
                         ))}
                     </div>
-                    <p className="pt-2 pb-16 w-[28.5rem] font-inter text-base font-normal" dangerouslySetInnerHTML={{ __html: words.email }} />
+                    <p className="pb-9 w-[15rem] font-inter text-xs font-normal ml-20
+                                  lg:ml-0 lg:w-[28.5rem] lg:text-base lg:pt-2 lg:pb-16
+                                  xl:ml-0 xl:w-[28.5rem] xl:text-base xl:pt-2 xl:pb-16" dangerouslySetInnerHTML={{ __html: words.email }} />
                 </div>
             </div>
-            <img src="Group 102.svg" />
+            <img className="w-[9rem] mr-20 lg:w-[20rem] lg:mr-0 xl:w-[20rem] xl:mr-0" src="Group 102.svg" />
         </div>
     )
 }
