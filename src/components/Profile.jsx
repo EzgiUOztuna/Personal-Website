dc966c8f4452a577d3be2d5e8b692c8d1cce8ba8
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-01-02 15:43:53 +0300

    Profile Responsive

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,20 +9,47 @@ export default function Profile() {
     return (
         <>
             <div className={`w-screen h-auto bg-gray-100 relative transition-all block ${darkMode ? "dark:bg-slate-800 text-white" : ""}`}>
-                <img className="absolute right-0 top-[-2rem]" src="Ellipse 12.svg" />
-                <h3 className="text-center font-inter text-[2.25rem] font-medium py-[3rem]">{words.profile}</h3>
-                <div className="flex justify-center items-center gap-16 pb-20">
-                    <div className={`border border-white rounded-lg bg-white shadow-[rgba(82,82,82,0.5)] w-[30rem] p-8 ${darkMode ? "dark:bg-gray-100 text-black" : ""}`}>
-                        <h4 className="font-playfair text-[#ea2678] font-normal text-[1.5rem] pb-[1.5rem] ml-[1rem]">{words.basicInformation}</h4>
-                        <p className="flex justify-start items-baseline my-2 font-inter text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.birthDate.label}</span>{words.birthDate.value}</p>
-                        <p className="flex justify-start items-baseline my-2 font-inter text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.city.label}</span>{words.city.value}</p>
-                        <p className="flex justify-start items-baseline my-2 font-inter text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4 ">{words.education.label}</span>{words.education.value}</p>
-                        <p className="flex justify-start items-baseline my-2 font-inter text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.role.label}</span>{words.role.value}</p>
+                <img className="absolute right-0 top-[-1.5rem] w-12
+                                lg:top-[-2rem] lg:w-[4.87rem]
+                                xl:top-[-2rem] xl:w-[4.87rem]" src="Ellipse 12.svg" />
+                <h3 className="text-center font-inter font-medium py-6 text-[1.5rem]
+                               lg:text-[2.25rem] lg:py-[3rem]
+                               xl:text-[2.25rem] xl:py-[3rem]">{words.profile}</h3>
+
+                <div className="flex flex-col justify-center items-center gap-10 pb-20
+                                lg:flex-row lg:gap-16
+                                xl:flex-row	xl:gap-16">
+                    <div className={`border border-white rounded-lg bg-white shadow-[rgba(82,82,82,0.5)] w-[24rem] p-3 
+                                    lg:w-[30rem] lg:p-8 
+                                    xl:w-[30rem] xl:p-8 ${darkMode ? "dark:bg-gray-100 text-black" : ""}`}>
+                        <h4 className="font-playfair text-[#ea2678] font-normal text-[1.2rem] pb-[1.5rem] ml-[1rem]
+                                       lg:text-[1.5rem]
+                                       xl:text-[1.5rem]">{words.basicInformation}</h4>
+                        <p className="flex justify-start items-baseline my-2 font-inter text-[1rem]
+                                      lg:text-[1.12rem]
+                                      xl:text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.birthDate.label}</span>{words.birthDate.value}</p>
+                        <p className="flex justify-start items-baseline my-2 font-inter text-[1rem]
+                                      lg:text-[1.12rem]
+                                      xl:text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.city.label}</span>{words.city.value}</p>
+                        <p className="flex justify-start items-baseline my-2 font-inter text-[1rem]
+                                      lg:text-[1.12rem]
+                                      xl:text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4 ">{words.education.label}</span>{words.education.value}</p>
+                        <p className="flex justify-start items-baseline my-2 font-inter text-[1rem]
+                                      lg:text-[1.12rem]
+                                      xl:text-[1.12rem]"><span className="font-semibold min-w-40 text-left ml-4">{words.role.label}</span>{words.role.value}</p>
                     </div>
 
-                    <div className="w-[27.5rem] leading-8 relative">
-                        <h4 className="font-playfair text-2xl font-normal leading-8 relative z-[10] ">{words.aboutMe} <img className="absolute top-[1.1rem] left-[-0.9rem] w-[5.62rem] -z-10" src="Rectangle 39.svg" /></h4>
-                        <p className="relative font-inter text-base font-normal my-6 w-[30rem]">{words.aboutMeParagraph}</p>
+                    <div className="w-[24rem] leading-8 relative p-3
+                                    lg:w-[27.5rem]
+                                    xl:w-[27.5rem]">
+                        <h4 className="font-playfair text-[1.2rem] font-normal leading-8 relative z-[10]
+                                       lg:text-2xl
+                                       xl:text-2xl">{words.aboutMe} <img className="absolute top-[1.1rem] left-[-0.7rem] w-[4.5rem] -z-10
+                                       lg:left-[-0.9rem] lg:w-[5.62rem]
+                                       xl:left-[-0.9rem] xl:w-[5.62rem]" src="Rectangle 39.svg" /></h4>
+                        <p className="relative font-inter text-base font-normal my-6 w-[23rem]
+                                      lg:w-[30rem]
+                                      xl:w-[30rem]">{words.aboutMeParagraph}</p>
                     </div>
                 </div>
             </div>
