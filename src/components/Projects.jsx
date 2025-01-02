d1446f9e7e684e93137a7ef84f23aa8a82717cf4
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-01-02 16:19:15 +0300

    Projects Responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,19 +10,29 @@ export default function Projects() {
     return (
         <>
             <div className={`w-screen h-auto relative transition-all duration-300 block ${darkMode ? "dark:bg-slate-800 text-white" : ""}`}>
-                <h1 className="text-center font-inter text-3xl font-medium py-12">{words.projects}</h1>
-                <div className={`flex justify-center gap-20 ${darkMode ? "dark:text-black" : ""}`}>
+                <h1 className="text-center font-inter font-medium py-6 text-[1.5rem]
+                               lg:py-12 lg:text-3xl
+                               xl:py-12 xl:text-3xl">{words.projects}</h1>
+                <div className={`flex flex-col justify-center items-center 
+                                 lg:flex-row lg:gap-20
+                                 xl:flex-row xl:gap-20 ${darkMode ? "dark:text-black" : ""}`}>
                     {words.project.map((item, index) => (
                         <div className={`border ${index === 0 ? 'border-[#DDEEFE] bg-[#DDEEFE]' : 'border-[#D9F6F1] bg-[#D9F6F1]'
-                            } rounded-lg w-[27rem] h-[42rem] p-8 mb-20 relative`}
+                            } rounded-lg h-[38.8rem] mb-20 relative w-[24rem] p-5
+                            lg:w-[27rem] lg:p-8 lg:mb-20 lg:h-[42rem]
+                            xl:w-[27rem] xl:p-8 xl:mb-20 xl:h-[42rem]`}
                             key={index}>
-                            <h2 className="font-playfair font-bold text-[1.8rem] pb-4">{item.title}</h2>
-                            <p className="font-inter font-normal text-[1rem] pb-4 w-[23rem]">{item.definition}</p>
+                            <h2 className="font-playfair font-bold pb-4 text-[1.3rem]
+                                           lg:text-[1.8rem]
+                                           xl:text-[1.8rem]">{item.title}</h2>
+                            <p className="font-inter font-normal text-[1rem] pb-4 w-[22.5rem]">{item.definition}</p>
                             <div className="flex gap-2 flex-wrap font-playfair font-bold justify-start">
                                 {(item.title === "Pizza Project" || item.title === "Pizza Projesi") && (
                                     <div className="flex gap-2 flex-wrap font-playfair font-bold justify-start">
                                         {words.pizzaProjectTechnologies && words.pizzaProjectTechnologies.map((tech, index) => (
-                                            <p key={index} className="border border-white rounded-full px-4 bg-white">
+                                            <p key={index} className="border border-white rounded-full px-4 bg-white text-[0.8rem]
+                                                                      lg:text-base
+                                                                      xl:text-base">
                                                 {tech}
                                             </p>
                                         ))}
@@ -31,31 +41,51 @@ export default function Projects() {
                                 {(item.title === "WITFLIX_Wiframe Project" || item.title === "WITFLIX_Wiframe Projesi") && (
                                     <div className="flex gap-2 flex-wrap font-playfair font-bold justify-start">
                                         {words.witflixProjectTechnologies && words.witflixProjectTechnologies.map((tech, index) => (
-                                            <p key={index} className="border border-white rounded-full px-4 bg-white">
+                                            <p key={index} className="border border-white rounded-full px-4 bg-white text-[0.8rem]
+                                                                      lg:text-base
+                                                                      xl:text-base">
                                                 {tech}
                                             </p>
                                         ))}
                                     </div>
                                 )}
                             </div>
-                            <div className="flex justify-between font-inter font-semibold text-[1.1rem] mb-16 pt-6 text-left">
+                            <div className="flex justify-between font-inter font-semibold text-[1rem] mb-0 pt-6 text-left
+                                            lg:text-[1.1rem] lg:mb-16
+                                            xl:text-[1.1rem] xl:mb-16">
                                 <a href={item.githubLink} target="_blank">
                                     <button>{words.viewGit}</button></a>
 
                                 <a href={item.appLink} target="_blank">
                                     <button>{words.goApp}<span>&#8594;</span></button></a>
                             </div>
-                            <img className="absolute top-[28.85rem] left-0" src={item.imageUrl} />
+                            <img className="absolute top-[27rem] left-0
+                                            lg:top-[28.85rem]
+                                            xl:top-[28.85rem]" src={item.imageUrl} />
                         </div>
                     ))}
                 </div>
-                <div className="flex justify-center items-center gap-20 pb-20 relative">
-                    <p className="font-inter font-medium text-[2.6rem] w-[28rem] text-right relative z-10">{words.footerP}<img className="absolute bottom-[4.1rem] left-[43%] w-60 z-[-1]" src="Rectangle 40.svg" /></p>
+                <div className="flex justify-center items-center gap-9 pb-20 relative px-7
+                                lg:gap-20 lg:px-0
+                                xl:gap-20 xl:px-0">
+                    <p className="font-inter font-medium text-[1.8rem] w-[24rem] text-right relative z-10
+                                  lg:text-[2.6rem] lg:w-[28rem]
+                                  xl:text-[2.6rem] xl:w-[28rem]">{words.footerP}<img className="absolute bottom-[5.5rem] left-[35%] w-40 z-[-1]
+                                  lg:w-60 lg:bottom-[4.1rem] lg:left-[43%]
+                                  xl:w-60 xl:bottom-[4.1rem] xl:left-[43%]" src="Rectangle 40.svg" /></p>
                     <ul>
-                        <a className="flex flex-col	text-base font-inter" style={{ color: "#1769FF" }} href={words.githubUrl} target="_blank">Github</a>
-                        <a className="flex flex-col	text-base font-inter" href={words.personalBlogUrl} target="_blank">{words.personalBlogTitle}</a>
-                        <a className="flex flex-col	text-base font-inter" style={{ color: "#0077B5" }} href={words.linkedinUrl} target="_blank">Linkedin</a>
-                        <a className="flex flex-col	text-base font-inter" style={{ color: "#AF0C48" }} href={words.emailUrl} target="_blank">Email</a>
+                        <a className="flex flex-col	text-[0.9rem] font-inter
+                                      lg:text-base
+                                      xl:text-base" style={{ color: "#1769FF" }} href={words.githubUrl} target="_blank">Github</a>
+                        <a className="flex flex-col font-inter text-[0.9rem]
+                                     lg:text-base
+                                    xl:text-base" href={words.personalBlogUrl} target="_blank">{words.personalBlogTitle}</a>
+                        <a className="flex flex-col font-inter text-[0.9rem]
+                                     lg:text-base
+                                    xl:text-base" style={{ color: "#0077B5" }} href={words.linkedinUrl} target="_blank">Linkedin</a>
+                        <a className="flex flex-col font-inter text-[0.9rem]
+                                      lg:text-base
+                                      xl:text-base" style={{ color: "#AF0C48" }} href={words.emailUrl} target="_blank">Email</a>
                     </ul>
                 </div >
             </div>
