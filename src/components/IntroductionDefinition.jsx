b5f9495aaf6cbcacb6c09788358f6b34e3715358
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-01-02 17:01:42 +0300

    Update IntroductionDefinition.jsx

--- src/components/IntroductionDefinition.jsx
+++ src/components/IntroductionDefinition.jsx
@@ -13,7 +13,7 @@ function IntroductionDefinition() {
                             lg:w-[40rem] lg:text-[2.62rem]
                             xl:w-[40rem] xl:text-[2.62rem]">
                 <img className="absolute left-[4.9rem] top-[2.8rem] z-[-1] h-5 w-[3.75rem]
-                                md:top-[6.2rem] md:h-[1.94rem] md:left-[-1.5rem] md:w-[9.25rem]
+                                md:top-[4.7rem] md:h-[1.94rem] md:left-[-2.4rem] md:w-[9.25rem]
                                 lg:top-[6.2rem] lg:h-[1.94rem] lg:left-[-1.5rem] lg:w-[9.25rem]
                                 xl:top-[6.2rem] xl:h-[1.94rem] xl:left-[-1.5rem] xl:w-[9.25rem]" src="Rectangle 2.svg" />
                 <p className="w-[12.5rem] ml-20 
