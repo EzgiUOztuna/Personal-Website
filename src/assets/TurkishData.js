f4876b78ba3c875c69a8fb2411cd18df7c10062c
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-04-02 15:35:46 +0300

    Update TurkishData.js

--- src/assets/TurkishData.js
+++ src/assets/TurkishData.js
@@ -5,7 +5,7 @@ export const turkishWords = {
     },
     translate: "<span style='color: #e92577;'>TÜRKÇE</span>'YE ÇEVİR",
     title: "Merhaba! 👋",
-    definition: "Ben Ezgi. Ben bir full-stack geliştiricisiyim. Sağlam ve ölçeklenebilir ön uç ürünler geliştirebilirim. Tanışalım!",
+    definition: "Ben Ezgi. Ben bir frontend geliştiricisiyim. Sağlam ve ölçeklenebilir ön uç ürünler geliştirebilirim. Tanışalım!",
     email: `Şu anda <span style="color: #AF0C48">Freelance</span> olarak <span style="color: #AF0C48">UX, UI ve Web Tasarımı</span> projeleri üzerinde çalışıyorum. Beni ekibinize davet edin <span>&#8594;</span> <a style="color: #AF0C48; text-decoration: underline" href="mailto:ezgiuygn@gmail.com" target="_blank">ezgiuygn@gmail.com</a>`,
     socialMedia: [
         {
