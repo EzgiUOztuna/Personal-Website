c41e0aaad7c86d19fabe7997a7f5508b7f189fa9
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-04-02 15:35:44 +0300

    Update EnglishData.js

--- src/assets/EnglishData.js
+++ src/assets/EnglishData.js
@@ -5,7 +5,7 @@ export const englishWords = {
     },
     translate: "TRANSLATE TO <span style='color: #e92577;'>ENGLISH</span>",
     title: "Hi! 👋",
-    definition: "I’m Ezgi. I’m a full-stack developer. I can craft solid and scalable frontend products. Let’s meet!",
+    definition: "I’m Ezgi. I’m a frontend developer. I can craft solid and scalable frontend products. Let’s meet!",
     email: `Currently <span style="color: #AF0C48">Freelancing</span> for <span style="color: #AF0C48">UX, UI, & Web Design</span> Projects. Invite me to join your team <span>&#8594;</span> <a style="color: #AF0C48; text-decoration: underline" href="mailto:ezgiuygn@gmail.com" target="_blank">ezgiuygn@gmail.com</a>`,
     socialMedia: [
         {
